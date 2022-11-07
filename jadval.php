4f5e5c5c5a21fc95c9ad42997fff29577ae5a687
alighadi <117599554+Niushaaaa@users.noreply.github.com>
2022-11-07 05:51:00 +0330

    نوشتن دستور تيبل
    نوشتن كد كلي اچ تي ام ال براي درست كردن صورت كلي جدول از جمله بادي هد تايتل و.......

--- jadval.php
+++ jadval.php
@@ -1,6 +1,18 @@
+<html>
+<head>
+    <title>
+    </title>
+</head>
+<body>
+<table border="3">
 <?php
 for ($a = 1; $a <= 10; $a++) {
     for ($b = 1; $b <= 10; $b++) {
         echo $a * $b;
     }
 }
+?>
+</table>
+</body>
+</html>
+    
